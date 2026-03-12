eacd0a2dbb1b9173aaa7b635f6f032814bf09d8c
Santhosh Kumar R <ss4856026@gmail.com>
2026-03-12 15:08:12 +0530

    mobile-view-video-issue-fixed-santhosh

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,13 @@
-	$(".js-height-full").height($(window).height());
-	$(".js-height-parent").each(function() {
-	    $(this).height($(this).parent().first().height());
+	function setFullHeights() {
+	    $(".js-height-full").height($(window).height());
+	    $(".js-height-parent").each(function() {
+	        $(this).height($(this).parent().first().height());
+	    });
+	}
+
+	setFullHeights();
+	$(window).on("resize orientationchange", function() {
+	    setFullHeights();
 	});
 
 
